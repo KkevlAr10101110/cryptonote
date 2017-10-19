10eef732724b72498453e19e65a4274df645808a
KkevlAr10101110 <32793931+KkevlAr10101110@users.noreply.github.com>
2017-10-19 12:46:57 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "192.168.11.222:16369",
+  "192.168.11.16:16369",
   //"youxxxxxxxxxxxxxxx",
 };
 
